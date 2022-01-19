6f77544fa4c0851533e8d9c9de50a7e88079fd67
Virgilio MENDOZA <virgilio.mendoza@amadeus.com>
2022-01-19 16:29:54 +0800

    Made modules - fixed

--- src/app/components/task-detail/task-detail.module.ts
+++ src/app/components/task-detail/task-detail.module.ts
@@ -4,7 +4,9 @@ import { CommonModule } from '@angular/common';
 import { TaskDetailComponent } from './task-detail.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TaskDetailComponent,
+  ],
   imports: [
     CommonModule
   ],
--- src/app/components/tasks/tasks.module.ts
+++ src/app/components/tasks/tasks.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { TasksComponent } from './tasks.component';
 import { TaskDetailModule } from '../task-detail/task-detail.module';
+import { TaskDetailComponent } from '../task-detail/task-detail.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TasksComponent,
+  ],
   imports: [
     CommonModule,
     TaskDetailModule
   ],
   exports: [
-    TasksComponent,
+    TasksComponent
   ]
 })
 export class TasksModule { }
