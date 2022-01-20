05d28b5f849d980e3ebce3396f8c6da9cadb16b2
Virgilio MENDOZA <virgilio.mendoza@amadeus.com>
2022-01-20 11:56:28 +0800

    Added some styling

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -3,4 +3,6 @@ import { Task } from "./task";
 export const TASKS: Task[] = [
     {id: 1, name: "Test", description: "This is a test"},
     {id: 2, name: "Test 2", description: "This is another test"},
+    {id: 3, name: "Test 3", description: "This is also another test"},
+    {id: 4, name: "Test 4", description: "This is yet another test"},
 ]
