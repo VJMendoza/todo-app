c65d90c5050f70fa52968b3e200e6b57ca161efc
Virgilio MENDOZA <virgilio.mendoza@amadeus.com>
2022-01-26 10:50:36 +0800

    Added base form elements

--- src/app/components/add-task/add-task.module.ts
+++ src/app/components/add-task/add-task.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 import { AddTaskComponent } from './add-task.component';
 
 @NgModule({
@@ -10,7 +17,13 @@ import { AddTaskComponent } from './add-task.component';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   exports: [
     AddTaskComponent,
